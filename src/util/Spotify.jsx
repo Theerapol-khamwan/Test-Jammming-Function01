68fc1dbf6013ed62ec549490deecb2f31ad3f8cd
Theerapol Khamwan <theerapol.khamwan@gmail.com>
2022-11-15 11:56:42 +0700

    first commit

--- src/util/Spotify.jsx
+++ src/util/Spotify.jsx
@@ -1,6 +1,8 @@
 let accessToken;
 const clientId = "060a3da96fdc4e8bbb7e171cdb72e09f";
-const redirectUri = "http://localhost:5173/";
+// const redirectUri = "http://localhost:5173/";
+const redirectUri = "https://jamming-miniproject-01.pages.dev/";
+
 const Spotify = {
   getAccessToken() {
     if (accessToken) {
@@ -49,7 +51,6 @@ const Spotify = {
           album: track.album.name,
           uri: track.uri,
         }));
-        // returns the response in an array with objects
       } else {
         throw new Error("search request failed");
       }
@@ -74,7 +75,6 @@ const Spotify = {
       if (response.ok) {
         const jsonResponse = await response.json();
         userId = jsonResponse.id;
-        // first gathers the user id before we can save the playlist
       } else {
         throw new Error("user Id request failed");
       }
@@ -93,17 +93,12 @@ const Spotify = {
       } else {
         throw new Error("playlist id request failed");
       }
-
-      // playListresponse is awaiting on the user id but once retrieved it can post the playlist name and then retrieve the playlist id
-
       const addPlaylistResponse = await fetch(
         `https://api.spotify.com/v1/users/${userId}/playlists/${playListId}/tracks`,
         {
           headers: headers,
           method: "POST",
           body: JSON.stringify({ uris: trackUris }),
-
-          // once userid is retrieved and the playlistname is posted(returning playlistid) we can now save the tracks under the playlist via a post method
         }
       );
     } catch (error) {
